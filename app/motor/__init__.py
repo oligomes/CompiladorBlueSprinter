a7752c3ce037d468bf43f480a9dda6e825dc6899
Gabriel Casarin da Silva <gabriel.casarin1@ibm.com>
2018-12-23 13:59:00 -0200

    new file:   app/motor/evento.py

--- app/motor/__init__.py
+++ app/motor/__init__.py
@@ -1,1 +1,2 @@
 from .motor_eventos import MotorEventos
+from .evento import Evento
---
+++ app/motor/evento.py
@@ -0,0 +1,4 @@
+class Evento:
+    def __init__(self, tipo, informacao=''):
+        self.tipo = tipo
+        self.informacao = informacao
